edc559c52608c6a327cd7ee7a3dbb842ca89af85
unknown <>
2021-01-16 20:57:19 +0300

    v 0.1

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ if(port == "" || port == null){
   port = 5000;
 }
 app.listen(port, function(){
-  console.log("en güncel port numarasi : " + port);
+  console.log("port numarasi : " + port);
 });
